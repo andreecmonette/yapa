44adc4b647ac3d61cee6d748168628c9d513f640
Andree Monette <andreecmonette@gmail.com>
2014-03-10 17:39:20 -0400

    sketched out collision checker

--- plinko.js
+++ plinko.js
@@ -16,15 +16,15 @@ controls = new THREE.OrbitControls(camera);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x33ff33, 1);
+renderer.setClearColor(0xccbbcc, 1);
 //no idea what that 1 is
 
 document.body.appendChild(renderer.domElement);
 var yScale = 1;
 var xScale = 1;
-var pegsGeom = new THREE.CylinderGeometry(.1, .1, 1, 16);
+var pegsGeom = new THREE.CylinderGeometry(.1, .1, 1, 8);
 var pegMaterial = new THREE.MeshBasicMaterial( {color: 0x111111} );
-var ballGeom = new THREE.SphereGeometry(.8, 16, 16);
+var ballGeom = new THREE.SphereGeometry(.4, 16, 16);
 var ballMaterial = new THREE.MeshBasicMaterial( {color: 0x333333} );
 var shiftedRight = true;
 var pegRows = 30;
@@ -40,22 +40,39 @@ for (var i = 0; i < pegRows; i++) {
     peg.position.x = (j+(shiftedRight-.5-pegCols)/2)*xScale;
     peg.rotation.x = Math.PI/2;
     scene.add(peg);
-    pegs.[i+j*pegRows] = peg;
-    pegPositions[i+j*pegRows] = 
+    pegs[i+j*pegRows] = peg;
+    pegPositions[i+j*pegRows] = new THREE.Vector2(peg.position.x, peg.position.y); 
   }
 }
 
+var movers = [];
+
 var sphere = new THREE.Mesh(ballGeom, ballMaterial);
-scene.add(sphere);
-sphere.velocity = 4;
+sphere.velocity = new THREE.Vector2(0.01, 0.01);
+movers.push(sphere);
 // click, make a ball!
+scene.add(sphere);
 //
-function physics() {
-  
+function tryMove(mover) {
+  mover.position.x += mover.velocity.x;
+  mover.position.y += mover.velocity.y;
 }
 
+function physics(movers) {
+  for (move in movers) {
+    tryMove(movers[move]);
+
+  }  
+}
+
+function checkCollisions(object, targets) {
+  // given two vectors, one can create a parametrized distance function between them
+  // deterministic 
+}  
+
+
 function render() {
-  physics();
+  physics(movers);
   requestAnimationFrame(render);
   renderer.render(scene, camera);
 }
